70f5ebcbfe6dab4d2ca2cb44785398baee8cb48e
Gujja, Reddy <NGujja@wrberkley.com>
2018-03-24 16:37:44 -0500

    added new endpoint to include projects to user

--- src/main/java/org/dsmhack/controller/UserController.java
+++ src/main/java/org/dsmhack/controller/UserController.java
@@ -1,6 +1,10 @@
 package org.dsmhack.controller;
 
+import org.dsmhack.model.Project;
 import org.dsmhack.model.User;
+import org.dsmhack.model.UserProject;
+import org.dsmhack.repository.ProjectRepository;
+import org.dsmhack.repository.UserProjectRepository;
 import org.dsmhack.repository.UserRepository;
 import org.dsmhack.service.CodeGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,12 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserProjectRepository userProjectRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @Autowired
     private CodeGenerator codeGenerator;
@@ -32,4 +42,32 @@ public class UserController {
         user.setUserGuid(codeGenerator.generateUUID());
         return userRepository.save(user);
     }
+
+    @GetMapping("/users/{userGuid}/projects")
+    public User getUsersAndProjects(@PathVariable String userGuid) {
+        User user = userRepository.findOne(userGuid);
+
+        List<UserProject> userProjectList = userProjectRepository.findAll();
+        userProjectList.forEach(userProject -> {
+            if(userProject.getMyKey().getUserGuid().equals(userGuid)){
+                Project project = projectRepository.findOne(userProject.getMyKey().getProjGuid());
+                user.getProjectList().add(project);
+            }
+        });
+        return user;
+    }
+
+//    @GetMapping("/users/{userId}/orgs")
+//    public User getUsersAndOrgs(@PathVariable String userGuid) {
+//        User user = userRepository.findOne(userGuid);
+//
+//        List<UserOrganization> userOrganizationList = userOrganizationRepository.findAll();
+//        userOrganizationList.forEach(userOrganization -> {
+//            if(userOrganization.getMyKey().getUserGuid().equals(userGuid)){
+//                Organization organization = organizationRepository.findOne(userOrganization.getMyKey().getOrgGuid());
+//                user.getOrganizationList().add(organization);
+//            }
+//        });
+//        return user;
+//    }
 }
