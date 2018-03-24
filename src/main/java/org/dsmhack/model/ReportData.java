051444212f185d6dfb867143e0e91c82dacd0528
Tony Brobston <anthonybrobston@gmail.com>
2018-03-24 13:46:21 -0500

    Adding a ReportData object and mapping to it

---
+++ src/main/java/org/dsmhack/model/ReportData.java
@@ -0,0 +1,11 @@
+package org.dsmhack.model;
+
+import java.sql.Timestamp;
+
+public class ReportData {
+    private String name = null;
+    private String firstName = null;
+    private String lastName = null;
+    private Timestamp timeIn = null;
+    private Timestamp timeOut = null;
+}
--- src/main/java/org/dsmhack/repository/ReportRepository.java
+++ src/main/java/org/dsmhack/repository/ReportRepository.java
@@ -1,24 +1,20 @@
 package org.dsmhack.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.dsmhack.model.CheckIn;
+import org.dsmhack.model.ReportData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
+import java.util.List;
 
 @Repository
-public class ReportRepository {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public java.util.List<Map<String, Object>> findAllReportingInformation(String organizationId) {
-        String sql =
-            "select project.name, user.first_name, user.last_name, check_in.time_in, check_in.time_out " +
+public interface ReportRepository extends JpaRepository<CheckIn, Long> {
+    @Query("select project.name as name, user.first_name as firstName, user.last_name as lastName, check_in.time_in as timeIn, check_in.time_out as timeOut " +
             "from organization, project, check_in, user " +
             "where organization.org_guid = project.org_guid " +
             "and project.proj_guid = check_in.proj_guid " +
             "and check_in.user_guid = user.user_guid " +
-            "and organization.org_guid = ?";
-        return jdbcTemplate.queryForList(sql, organizationId);
-    }
+            "and organization.org_guid = ?1")
+    List<ReportData> findAllReportingInformation(String organizationId);
 }
