1357504f1f758dfc0f2e337660b6be82750ea04e
Tony Brobston <anthonybrobston@gmail.com>
2018-03-24 12:55:25 -0500

    Adding a query to all reporting data

---
+++ src/main/java/org/dsmhack/repository/ReportRepository.java
@@ -0,0 +1,25 @@
+package org.dsmhack.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public class ReportRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public java.util.List<Map<String, Object>> findAllReportingInformation(String organizationId) {
+        String sql =
+            "select * " +
+            "from organization, project, check_in, user " +
+            "where organization.org_guid = project.org_guid " +
+            "and project.proj_guid = check_in.proj_guid " +
+            "and check_in.user_guid = user.user_guid " +
+            "and organization.org_guid = ?";
+        return jdbcTemplate.queryForList(sql, new Object[]{organizationId});
+    }
+
+}
