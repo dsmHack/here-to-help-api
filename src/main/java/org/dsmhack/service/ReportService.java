f1983cb1644573cfb332345a3e729675b6af6959
Tony Brobston <anthonybrobston@gmail.com>
2018-03-24 11:48:33 -0500

    Forgot to add @Service to a service

--- src/main/java/org/dsmhack/service/ReportService.java
+++ src/main/java/org/dsmhack/service/ReportService.java
@@ -1,7 +1,9 @@
 package org.dsmhack.service;
 
 import org.dsmhack.model.ReportOrganization;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReportService {
     public ReportOrganization getReportOrganization(String organizationId) {
         return null;
