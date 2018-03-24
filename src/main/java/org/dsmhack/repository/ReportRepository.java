117b87e828d39013334507dc901ec2d274df1535
Tony Brobston <anthonybrobston@gmail.com>
2018-03-24 13:11:12 -0500

    Refining columns in query

--- src/main/java/org/dsmhack/repository/ReportRepository.java
+++ src/main/java/org/dsmhack/repository/ReportRepository.java
@@ -13,13 +13,12 @@ public class ReportRepository {
 
     public java.util.List<Map<String, Object>> findAllReportingInformation(String organizationId) {
         String sql =
-            "select * " +
+            "select project.name, user.first_name, user.last_name, check_in.time_in, check_in.time_out " +
             "from organization, project, check_in, user " +
             "where organization.org_guid = project.org_guid " +
             "and project.proj_guid = check_in.proj_guid " +
             "and check_in.user_guid = user.user_guid " +
             "and organization.org_guid = ?";
-        return jdbcTemplate.queryForList(sql, new Object[]{organizationId});
+        return jdbcTemplate.queryForList(sql, organizationId);
     }
-
 }
