cc9e608b90338fcdc1c7cc49202746c22703b707
Tony Brobston <anthonybrobston@gmail.com>
2018-05-27 11:55:41 -0500

    Fixing some mistakes I made when fixing a merge conflict

--- src/main/java/org/dsmhack/config/AppUserDetailsService.java
+++ src/main/java/org/dsmhack/config/AppUserDetailsService.java
@@ -22,7 +22,7 @@ public class AppUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userGuid) throws UsernameNotFoundException {
         LoginToken securityCode = loginTokenRepository.findByUserGuid(userGuid);
-        boolean isTokenExpired = securityCode.getTokenExpDate().before(Timestamp.valueOf(LocalDateTime.now()));
+        boolean isTokenExpired = securityCode.getTokenExpDate().isBefore(LocalDateTime.now());
         if (securityCode == null) {
             throw new UsernameNotFoundException(userGuid);
         }
